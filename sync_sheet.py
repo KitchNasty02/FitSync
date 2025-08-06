983fd15093e92f6aa3ad8a806059b37cc780b527
KitchNasty02 <connorkitch10@gmail.com>
2025-08-06 10:24:26 -0600

    safe requests implemented

--- sync_sheet.py
+++ sync_sheet.py
@@ -1,21 +1,54 @@
-import gspread
-from gspread_formatting import *
-from gspread_formatting import TextFormat, CellFormat, format_cell_range
+from gspread_formatting import set_frozen
+from gspread.exceptions import APIError
 from collections import defaultdict
+from datetime import datetime
+import time
+
+
+# ensures the api request do not fail from the rate limit
+def safe_request(func, *args, max_sleep=60, **kwargs):
+    for sleep_time in [1, 2, 4, 8, 16, 32, max_sleep]:
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            if "429" in str(e):
+                print(f"Rate limit hit, trying again in {sleep_time}s...")
+                time.sleep(sleep_time)
+            else:
+                raise e
+
 
 # colors
 MONTH_COLOR = (207, 226, 243)
 HEADERS_COLOR = (159, 197, 232)
+ROW_COLORS = [
+    {"red": 0.98, "green": 0.98, "blue": 0.98},  # light gray
+    {"red": 1.0, "green": 1.0, "blue": 1.0}      # white
+]
+
+# convert to better labels in sheet
+TYPEKEY_MAP = {
+    "running": "Run",
+    "cycling": "Bike",
+    "swimming": "Swim",
+    "lap_swimming": "Swim",
+    "strength_training": "Strength",
+    "cardio_training": "Cardio",
+    "walking": "Walk",
+    "hiking": "Hike",
+    "treadmill_running": "Run",
+    "indoor_cycling": "Bike",
+    "other": "Other"
+}
 
 
 def update_header(sheet):
     # Add update for weekly mileage graphs and stuff
     # would shift the frozen rows down
 
-
     headers = ["", "Date", "Activity", "Distance", "Time", "Avg HR", "RPE", "Description"]
-    sheet.update("A1", [headers])
-    sheet.format("A1:H1", {
+    safe_request(sheet.update, "A1", [headers])
+    safe_request(sheet.format, "A1:H1", {
         "horizontalAlignment": "CENTER", 
         "backgroundColor": rgb_to_normalized(HEADERS_COLOR),
         "textFormat": {
@@ -23,16 +56,13 @@ def update_header(sheet):
             "bold": True,
             "fontFamily": "Times New Roman"}
         })
-    set_frozen(sheet, rows=1)
+    safe_request(set_frozen, sheet, rows=1)
 
 
 def sync_sheet(sheet, workout_data):
 
     top_row = sheet.row_values(2)
     latest_date = top_row[1] if len(top_row) > 1 else None
-
-    # existing_values = sheet.get_all_values()
-    # existing_dates = [row[1] for row in existing_values[1:] if len(row) > 1]
 
     # only get workout with newer dates
     unsynced = [
@@ -47,27 +77,24 @@ def sync_sheet(sheet, workout_data):
     
     daily_groups = defaultdict(list)
     for w in unsynced:
-        daily_groups[w["startTimeLocal"]].append(w)
+        if isinstance(w["startTimeLocal"], str):
+            w["startTimeLocal"] = datetime.strptime(w["startTimeLocal"], "%Y-%m-%d %H:%M:%S")
+
+        workout_date = w["startTimeLocal"].date()
+        daily_groups[workout_date].append(w)
 
     # insert new row at top after header
     insert_index = 2
     last_month = None
+
+    color_index = 0
+
     for date in sorted(daily_groups.keys(), reverse=True):
-        
+
         # check month change
         current_month = date.strftime('%Y-%m')
         if current_month != last_month:
-            divider_text = date.strftime('%B %Y')
-            sheet.insert_row([divider_text], index=insert_index)
-            sheet.merge_cells(f"A{insert_index}:H{insert_index}")
-
-            # Apply formatting: bold, left-aligned, gray fill
-            sheet.format(f"A{insert_index}:G{insert_index}", {
-                "backgroundColor": rgb_to_normalized(MONTH_COLOR),
-                "horizontalAlignment": "LEFT",
-                "textFormat": {"fontSize": 10, "bold": True}
-            })
-
+            insert_month_divider(sheet, date, insert_index)
             insert_index += 1
             last_month = current_month
 
@@ -76,36 +103,59 @@ def sync_sheet(sheet, workout_data):
         start_row = insert_index
 
         for i, workout in enumerate(workouts):
-            # TODO: update this based on how the garmin data is set up (not the testing data)
-            row = [
-                "",
-                date.strftime('%m/%d') if i == 0 else "",  
-                workout.get("sportType", {}).get("typeKey", "").title(), 
-                round(workout.get("distance", 0) / 1609.34, 2), 
-                round(workout.get("duration", 0) / 60), 
-                workout.get("averageHR", ""),
-                workout.get("description", "")
-            ]
-
-            sheet.insert_row(row, index=insert_index)
-
-            # Format newly inserted row
-            sheet.format(f"A{insert_index}:G{insert_index}", {
-                "backgroundColor": {"red": 1, "green": 1, "blue": 1},
-                "textFormat": {"fontSize": 10}
-            })
-
+            insert_row(sheet, workout, date, insert_index, i == 0, row_color=ROW_COLORS[color_index])
             insert_index += 1
 
         # Merge date cells across grouped rows
         if len(workouts) > 1:
-            sheet.merge_cells(f"B{start_row}:B{insert_index - 1}")
-            sheet.format(f"B{start_row}", {"verticalAlignment": "MIDDLE"})
+            safe_request(sheet.merge_cells, f"B{start_row}:B{insert_index - 1}")
+            safe_request(sheet.format, f"B{start_row}", {"verticalAlignment": "MIDDLE"})
 
-    # center text
-    sheet.format("B:G", {"horizontalAlignment": "CENTER"})
+        color_index = 1 - color_index
 
     print(f"Inserted {sum(len(v) for v in daily_groups.values())} new workouts.")
+
+
+def insert_month_divider(sheet, date, insert_index, max_sleep = 60):
+    divider_text = date.strftime('%B %Y')
+    sleep_time = 1
+    
+    safe_request(sheet.insert_row, [divider_text], index=insert_index)
+    safe_request(sheet.merge_cells, f"A{insert_index}:H{insert_index}")
+
+    safe_request(sheet.format, f"A{insert_index}:G{insert_index}", {
+        "backgroundColor": rgb_to_normalized(MONTH_COLOR),
+        "horizontalAlignment": "LEFT",
+        "textFormat": {"fontSize": 10, "bold": True}
+    })
+
+
+def insert_row(sheet, workout, date, insert_index, is_first_row, row_color, max_sleep=60):
+    
+    raw_type = workout.get("sportType", {}).get("typeKey", "")
+    activity_label = TYPEKEY_MAP.get(raw_type, raw_type.title())
+
+    row = [
+        "",
+        date.strftime('%m/%d') if is_first_row else "",  
+        activity_label, 
+        round(workout.get("distance", 0) / 1609.34, 2), 
+        round(workout.get("duration", 0) / 60), 
+        workout.get("averageHR", ""),
+        "",
+        workout.get("description", "")
+    ]
+
+    sleep_time = 1
+    safe_request(sheet.insert_row, row, index=insert_index)
+
+    # Format new row
+    safe_request(sheet.format, f"A{insert_index}:H{insert_index}", {
+        "backgroundColor": row_color,
+        "horizontalAlignment": "CENTER",
+        "textFormat": {"fontSize": 10}
+    })
+    safe_request(sheet.format, f"H{insert_index}", {"horizontalAlignment": "LEFT"})
 
 
 
@@ -116,6 +166,7 @@ def rgb_to_normalized(rgb):
         "green": rgb[1] / 255,
         "blue": rgb[2] / 255
     }
+
 
 
 
